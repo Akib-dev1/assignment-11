1ad44a001c85b5b2ebe0423e512ca7cdfb407ed9
Akib-dev1 <akibanj2017@gmail.com>
2025-06-05 11:05:43 +0600

    My Reviews Done

--- src/MyReviewCard.jsx
+++ src/MyReviewCard.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import { useNavigation } from "react-router";
 
-const MyReviewCard = () => {
+const MyReviewCard = ({ review }) => {
+  const [service, setService] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:3000/services/${review.reviewFor}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setService(data);
+      });
+  }, [review.reviewFor]);
+  const navigation = useNavigation();
+  if (navigation.state === "loading") {
     return (
-        <div>
-            I am my review card component
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-bars loading-xl mx-auto"></span>
+      </div>
     );
+  }
+  return (
+    <div>
+      <div className="card bg-info card-lg shadow-sm">
+        <div className="card-body">
+          <div className="flex items-center gap-4">
+            <div className="avatar">
+              <div className="w-16 rounded">
+                <img
+                  src={review.userImage}
+                  alt="Tailwind-CSS-Avatar-component"
+                />
+              </div>
+            </div>
+            <h2 className="card-title">{review.userName}</h2>
+          </div>
+          <Rating
+            style={{ maxWidth: 180 }}
+            value={review.ratingStar}
+            readOnly
+          />
+          <p>{review.reviewText}</p>
+          <div className="justify-end card-actions">
+            <p className="font-semibold">
+              Review Added Date: {review.reviewDate}
+            </p>
+          </div>
+          <p className="font-semibold">Service Title: {service?.serviceTitle}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyReviewCard;
--- src/MyReviews.jsx
+++ src/MyReviews.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "./AuthProvidor";
+import MyReviewCard from "./MyReviewCard";
+import { useNavigation } from "react-router";
 
 const MyReviews = () => {
+  const { user } = use(AuthContext);
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:3000/reviews/forUser/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, [user?.email, reviews]);
+  const navigation = useNavigation();
+  if (navigation.state === "loading") {
     return (
-        <div>
-            I am My Reviews Page
-        </div>
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-bars loading-xl mx-auto"></span>
+      </div>
     );
+  }
+  return (
+    <div className="w-11/12 mx-auto py-10 min-h-screen">
+      <h1 className="text-3xl font-bold text-center mb-8 text-[#257459]">
+        My Reviews
+      </h1>
+      <div className="grid grid-cols-1 gap-6">
+        {reviews.map((review) => (
+          <MyReviewCard key={review._id} review={review} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyReviews;
